8745b9c121d58cc2e043e3970d6e1e766850be36
KuharG15 <KuharG15@R310-05.std.fmf.uni-lj.si>
2018-04-18 09:53:38 +0200

    Peterice

---
+++ Gomoku/src/Logika/Igra.java
@@ -0,0 +1,43 @@
+package Logika;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Igra {
+
+	// velikost igralne plošèe
+	public static final int N = 15;
+	
+	private static final List<Peterica> peterice = new LinkedList<Peterica>();
+	
+	// dolžina peterice
+	public static final int PET = 5;
+	
+	private Polje[][] plosca;
+	private Igralec prviNaPotezi;
+	
+	{	// Inicializiramo Peterice
+		
+		// Naredimo tabelo peteric
+		// smer navzdol desno +
+		int[][] smer = {{1,0}, {0,1}, {1,1}, {1,-1}};
+		for (int x = 0; x < N; x++) {
+			for (int y = 0; y < N; y++) {
+				for (int[] s : smer) {
+					int dx = s[0];
+					int dy = s[1];
+					if ((0 <= x + (PET-1) * dx) && (x + (PET-1) * dx < N) &&
+						(0 <= y + (PET-1) * dy) && (y + (PET-1) * dy < N)) {
+						int[] peterica_x = new int[5];
+						int[] peterica_y = new int[5];
+						for (int k = 0; k < PET; k++) {
+							peterica_x[0] = x + dx * k;
+							peterica_y[1] = y + dy * k;
+						}
+						peterice.add(new Peterica(peterica_x, peterica_y));
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Gomoku/src/Logika/Peterica.java
@@ -0,0 +1,13 @@
+package Logika;
+
+public class Peterica {
+
+	// Peterica predstavljena s seznamom koordinat
+	public int[] x;
+	public int[] y;
+	
+	public Peterica(int[] x, int y[]) {
+		this.x = x;
+		this.y = y;
+	}
+}
