cae048ba6aa0c6282c197d582c29fe92204ce0c3
KuharG15 <KuharG15@R310-05.std.fmf.uni-lj.si>
2018-04-25 10:06:03 +0200

    Dodal funkcije stanj

--- Gomoku/src/Logika/Stanje.java
+++ Gomoku/src/Logika/Stanje.java
@@ -10,7 +10,7 @@ public enum Stanje {
 	private int steviloBelih;
 	private int steviloCrnih;
 	
-	public Stanje prestej(Igra igra) {
+	public void prestej(Igra igra) {
 		steviloBelih = 0;
 		steviloCrnih = 0;
 		for (int i = 0 ; i < Igra.N ; i++) {
@@ -25,8 +25,8 @@ public enum Stanje {
 	}
 		
 	// še ni dokonèano
-	// naredi funkcije: veljavnost igre, kdo zmagal ali neodloèeno, kdo na potezi
-	public boolean veljavnost(Igra igra) {
+	// naredi funkcije: veljavnost igre (boolean), kdo zmagal ali neodloèeno, kdo na potezi
+	public Stanje veljavnost(Igra igra) {
 		if (steviloBelih + steviloCrnih == Igra.N * Igra.N) {
 			return NEODLOCENO;
 		}
