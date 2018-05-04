beacc6682fc37f65ea183b65f5700d24c97eac57
Gal Kuhar <gal.kuhar+git@gmail.com>
2018-05-04 21:49:27 +0200

    Pobrisal zakomentirano funkcijo. Closes #4

--- Gomoku/src/logika/Peterica.java
+++ Gomoku/src/logika/Peterica.java
@@ -11,28 +11,6 @@ public class Peterica {
 		this.y = y;
 	}
 	
-//	/*
-//	 * Vrne barvo polj ce je peterica polna, drugace vrne null
-//	 */
-//	public Polje preveri(Plosca plosca) {
-//		int steviloBelih = 0;
-//		int steviloCrnih = 0;
-//		for (int i = 0; i < Igra.PET; i++) {
-//			if (plosca.element(x[i], y[i]) == Polje.BELI) {
-//				steviloBelih++;
-//			} else if (plosca.element(x[i], y[i]) == Polje.CRNI) {
-//				steviloCrnih++;
-//			}
-//		}
-//		if (steviloBelih == 5) {
-//			return Polje.BELI;
-//		} else if (steviloCrnih == 5) {
-//			return Polje.CRNI;
-//		} else {
-//			return null;
-//		}
-//	}
-	
 	// ta metoda hitrejsa, saj ni treba do konca gledati peterice - takoj ko najdes eno polje drugacno lahko koncas.
 	/**
 	 * 
